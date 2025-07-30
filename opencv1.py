33647661ebca90b301a765ee1928138200f3fb83
Ebrar Cemre Çetin <ebrarcemre20mail.com>
2025-07-30 16:33:54 +0300

    İlk commit

---
+++ opencv1.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+img=cv2.imread("resimler/channels4_profile.jpg")
+cv2.line (img,(0,0),(511,511),(255,0,0),5)
+cv2.rectangle (img,(384,0),(510,128),(0,255,0),3)
+cv2.circle (img,(555,73), 63, (0,0,0), 0)
+cv2.ellipse (img,(800,500),(100,50),0,0,170,255,-2)
+pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
+pts = pts.reshape([-1,1,2])
+cv2.polylines(img,[pts],True,(0,255,255))
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,'My Best Singer',(10,400), font, 3,(255,255,255),2,cv2.LINE_AA)
+
+cv2.imshow("image",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2 .imwrite("resimler/image.jpeg",img)
+
