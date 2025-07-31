3405d61f631306a6fa0afc7042b322331f6bda9d
Ebrar Cemre Çetin <ebrarcemre20mail.com>
2025-07-31 11:12:31 +0300

    change text coordinate

--- opencv1.py
+++ opencv1.py
@@ -9,7 +9,7 @@ pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
 pts = pts.reshape([-1,1,2])
 cv2.polylines(img,[pts],True,(0,255,255))
 font = cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(img,'My Best Singer',(10,400), font, 3,(255,255,255),2,cv2.LINE_AA)
+cv2.putText(img,'My Best Singer',(100,400), font, 3,(255,255,255),2,cv2.LINE_AA)
 
 cv2.imshow("image",img)
 cv2.waitKey(0)
